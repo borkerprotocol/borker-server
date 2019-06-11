9b0b0084aa22ef8ce433bc1a76663c82bc6d425d
Aiden McClelland <aiden.mcclelland@saltlending.com>
2019-06-11 12:01:02 -0600

    ssl

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,19 @@ const PORT = process.env.PORT || 4422
 
 createConnection()
 	.then(() => {
-		app.listen(PORT, () => {
-      console.log(`borker listening on port ${ PORT }`)
-      syncChain()
+		let a = app
+		const cfg = require('../borkerconfig.json')
+		if (cfg.cert && cfg.key) {
+			const https = require('https')
+			const fs = require('fs')
+			a = https.createServer({
+				key: fs.readFileSync(cfg.key),
+				cert: fs.readFileSync(cfg.cert),
+			}, app)
+		}
+		a.listen(PORT, () => {
+			console.log(`borker listening on port ${PORT}`)
+			syncChain()
 		})
 	})
 	.catch(err => console.log(err))
